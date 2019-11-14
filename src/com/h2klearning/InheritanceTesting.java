5b3f4632e1dab4c114699ed3e4600fb2e5892754
nratheesh <n.ratheesh2010@gmail.com>
2019-11-14 14:05:22 -0500

    created an instance of parent type

--- src/com/h2klearning/InheritanceTesting.java
+++ src/com/h2klearning/InheritanceTesting.java
@@ -5,11 +5,11 @@ public class InheritanceTesting {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Creating an instance of referencetype(parent)
-		//Customer pCustomer = new PreferredCustomer();
-		//System.out.println(pCustomer.firstName = "Neil");
+		Customer pCustomer = new PreferredCustomer();
+		System.out.println(pCustomer.firstName = "Neil");
 		//Creating an instance of child class
 		PreferredCustomer pCustomerSecond = new PreferredCustomer();
-		//System.out.println(pCustomerSecond.phNumber = "6543279081");
+		System.out.println(pCustomerSecond.phNumber = "6543279081");
 
 	}
 
